39ada9e64ea6faa5df823c94a596dce98b2ec533
alvaroyturburo <alvaroyturburo@hotmail.com>
2017-06-19 17:13:25 -0500

    Control proyectos modificaciones v2

--- src/ec/ed/upse/controlProyectosGad/controladores/Login.java
+++ src/ec/ed/upse/controlProyectosGad/controladores/Login.java
@@ -1,11 +1,14 @@
 package ec.ed.upse.controlProyectosGad.controladores;
 
+import java.util.Random;
+
 /*import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Formatter;*/
 
 import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
@@ -27,12 +30,14 @@ import lombok.extern.java.Log;
 @Log
 public class Login {
 
-	//@Wire private Textbox txtUsuario;
-	//@Wire private Textbox txtClave;
+	@Wire private Textbox txtC;
+	@Wire private Textbox txtCG;
 	
 	@Getter @Setter private String alias;
 	@Getter @Setter private String contrasena;
 	@Getter @Setter private String mensaje;
+	@Getter @Setter private String captchaGenerado;
+	@Getter @Setter private String captcha;
 
 	
 	private PdpsUsuarioDAO usuarioDao = new PdpsUsuarioDAO();
@@ -42,6 +47,8 @@ public class Login {
 	public void afterCompose(@ContextParam(ContextType.VIEW) Component view){
 		Selectors.wireComponents(view, this, false);
 		log.info("Login");
+		txtCG.setText(getCadenaAlfanumAleatoria(5));
+		//System.out.println(""+getCadenaAlfanumAleatoria(4));
 	}
 	
 	/*private static String encryptPassword(String password)
@@ -77,6 +84,22 @@ public class Login {
 	    return result;
 	}*/
 	
+	String getCadenaAlfanumAleatoria (int longitud){
+		String cadenaAleatoria = "";
+		long milis = new java.util.GregorianCalendar().getTimeInMillis();
+		Random r = new Random(milis);
+		int i = 0;
+		while ( i < longitud){
+		char c = (char)r.nextInt(255);
+		if ( (c >= '0' && c <='9') || (c >='A' && c <='Z') ){
+		cadenaAleatoria += c;
+		i ++;
+		}
+		}
+		return cadenaAleatoria;
+		}
+
+	
 	@Command
 	@NotifyChange({"mensaje"})
 	public void ingresar(){
@@ -84,12 +107,32 @@ public class Login {
 		//usuarioLogin = usuarioDao.login(txtUsuario.getText(), encryptPassword(txtClave.getText()));
 		if(usuarioLogin == null){
 			mensaje = "Error en las credenciales de acceso";
+			txtCG.setText(getCadenaAlfanumAleatoria(5));
+			txtC.setDisabled(false);
 		}else{
+			if(txtC.getText().equals(txtCG.getText())){
 			Session session;
 			session=Sessions.getCurrent();
 			session.setAttribute("_USUARIO_", usuarioLogin);
-			Executions.getCurrent().sendRedirect("index.zul");
+			Executions.getCurrent().sendRedirect("index.zul");}
+			else{
+				mensaje = "Error en la captcha";
+				txtCG.setText(getCadenaAlfanumAleatoria(5));
+				txtC.setDisabled(false);
+			}
 		}
+	}
+	
+	@Command
+	public void comprobar(@BindingParam("valor") String valor){
+		if(valor.equals(txtCG.getText().toString())){
+			txtC.setDisabled(true);
+		}
+	}
+	
+	@Command
+	public void reGenerar(){
+		txtCG.setText(getCadenaAlfanumAleatoria(5));
 	}
 }
 
